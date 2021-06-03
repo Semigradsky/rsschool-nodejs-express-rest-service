f5bb14eaf867474062af82aa13fb5eae9a823ed1
Dmitry Semigradsky <semigradskyd@gmail.com>
2021-06-03 12:37:41 +0300

    feat: add logger (winston)

---
+++ src/logger.ts
@@ -0,0 +1,31 @@
+import { createLogger, transports, format } from 'winston'
+
+const logger = createLogger({
+  level: 'debug',
+  transports: [
+    new transports.Console({
+      format: format.combine(
+        format.colorize(),
+        format.cli(),
+      ),
+    }),
+
+    new transports.File({
+      filename: 'debug.log',
+    }),
+
+    new transports.File({
+      filename: 'error.log',
+      format: format.json(),
+      level: 'error',
+    }),
+  ],
+});
+
+export const log = (message: string, meta?: object): void => {
+  logger.debug(message, meta);
+};
+
+export const errorLog = (message: string, meta?: object): void => {
+  logger.error(message, meta);
+};
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
+import { log } from 'logger';
 import { PORT } from './common/config';
 import app from './app';
 
 app.listen(PORT, () =>
-  console.log(`App is running on http://localhost:${PORT}`)
+  log(`App is running on http://localhost:${PORT}`)
 );
